2d781ed933d225e3fef17e19f40e0289236267c7
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-02 20:40:57 +0200

    [ADDITION] Choice Fields
    To limit the choice in a variable we can use choice filed. The notes for choice fields has also been added

--- README.md
+++ README.md
@@ -183,3 +183,26 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 ```
+
+### Choice Fields
+
+If a schema entity can have a limited choice, we can do that using choices. Inside the choices which only accepts a dictionary. We can also provide a default value. Here is the code for Customer model for more reference.
+
+```python
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(unique=True)
+    phone = models.IntegerField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+```
--- django-part1/store/models.py
+++ django-part1/store/models.py
@@ -8,8 +8,33 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(unique=True)
     phone = models.IntegerField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    placed_at = models.DateTimeField(auto_created=True)
+
+    PENDING = 'P'
+    COMPLETE = 'C'
+    FAILED = 'F'
+
+    PRICE_CHOICES = [
+        (PENDING, 'Pending'),
+        (COMPLETE, 'Complete'),
+        (FAILED, 'Complete')
+    ]
+
+    payment_status = models.Model(max_length=1, choices=PRICE_CHOICES, default=PENDING)
