628c23b752b591c6efb9f95931ca214486235ad5
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-08 22:57:27 +0200

    [UPDATE] Variables has been updated
    1. Label has been renamed to title
    2. The phone number field has been changed from integer field to char field

--- django-part1/store/models.py
+++ django-part1/store/models.py
@@ -31,7 +31,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(unique=True)
-    phone = models.IntegerField(max_length=255)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
