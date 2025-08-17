f939e8b03f536dc6c116f54bfb7f2945ff8b7e93
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-17 13:50:34 +0200

    [UPDATE] Deleting Objects

--- django-part1/playground/views.py
+++ django-part1/playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from store.models import Collection, Product
 
 def say_hello(request):
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    collection = Collection(pk=11)
+    collection.delete()
 
     return render(request, 'hello.html', {'name': 'Pritom'})
