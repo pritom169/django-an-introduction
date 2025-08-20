d3f69c566047e47852e17895ad892591f3028148
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-20 17:07:56 +0200

    [ADDITION] Incorporate readonly fields

--- django-part1/store/admin.py
+++ django-part1/store/admin.py
@@ -37,7 +37,7 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    exclude = ['promotions']
+    readonly = ['promotions']
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
