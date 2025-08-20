418f0405e21a80332ee5007ef3521ac6bc4eba9c
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-20 17:20:14 +0200

    [ADDITION] Pre-populating one field with another.

--- django-part1/store/admin.py
+++ django-part1/store/admin.py
@@ -37,7 +37,9 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    readonly = ['promotions']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
