27210065327bcac2ee96754425364307086662f6
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-20 17:55:59 +0200

    [ADDITION] Adding autocomplete for customers inside new order page

--- django-part1/store/admin.py
+++ django-part1/store/admin.py
@@ -87,4 +87,5 @@ class CustomerAdmin(admin.ModelAdmin):
         
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
     list_display = ['id', 'placed_at', 'customer']
