4b5c019e44910ee1b6aa10659e13bc51756fe376
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-27 19:02:32 +0200

    [ADDITION] Adding type validation for the input parameters

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<id>/', views.product_detail)
+    path('products/<int:id>/', views.product_detail)
 ]
