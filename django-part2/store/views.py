00c3e19f80ca4500cb6a697636d0f7bbc4046a34
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-28 23:44:23 +0200

    [ADDITION] Replacing try and catch blocks with get_object_or_404 query

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,9 +12,6 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
