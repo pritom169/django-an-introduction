e5956769341f79067aefca9265d68d2f1d196bd5
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-31 17:26:08 +0200

    [ADDITION] Applying ModelSerializer to Collection class

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -2,9 +2,10 @@ from decimal import Decimal
 from rest_framework import serializers
 from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
