f865e36d8d00c97a349825d75b3db809015a83ba
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-31 17:25:05 +0200

    [ADDITION] Adding extra field to model serializer in ProductSerializer

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -9,15 +9,8 @@ class CollectionSerializer(serializers.Serializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'collection']
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
-    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source="unit_price")
-    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.HyperlinkedRelatedField(
-    #     queryset = Collection.objects.all(),
-    #     view_name = 'collection-detail'
-    # )
+        fields = ['id', 'title', 'unit_price', 'price_with_tax' ,'collection']
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    # def calculate_tax(self, product: Product):
-    #     return product.unit_price * Decimal(1.1)
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
