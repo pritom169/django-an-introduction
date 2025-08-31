8077f7bb526bfeec3e6e41cd9afc2ba03aa436a5
Pritom Mazumder <pritommazumder@gmail.com>
2025-08-31 16:30:39 +0200

    [ADDITION] Including Hyperlink into the nested object

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail)
+    path('products/<int:id>/', views.product_detail),
+    path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
