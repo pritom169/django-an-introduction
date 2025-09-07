0bcc1e489377654c643d60a76c5132b96fd902e1
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-07 10:48:00 +0200

    [ADD] Updating the Product in Serializer

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -15,3 +15,14 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.other = 1
+        product.save()
+        return product
+    
+    def update(self, instance, validated_data):
+        instance.unit_price = validated_data.get('unit_price')
+        instance.save()
+        return instance
