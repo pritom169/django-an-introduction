347b34758ef54742db33c6427dc3a5edb6656760
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-13 22:21:43 +0200

    [ADD] ProductDetails converted from function based views to class based views

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.product_detail),
+    path('products/<int:id>/', views.ProductDetail.as_view()),
     path('collection/', views.collection_list),
     path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -18,24 +18,27 @@ class ProductList(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+
+class ProductDetail(APIView):
+    def get(self, request):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    
+    def put(self, request):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    
+    def delete(self, request):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response({'error' : 'Product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+        
 @api_view(['GET', 'POST'])
 def collection_list(request):
     if request.method == 'GET':
