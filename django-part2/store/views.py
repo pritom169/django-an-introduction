e7306269aac89ccf5682ebc2796ca4ff605a057f
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-14 15:14:09 +0200

    [ADD] Manual get and post functions has been removed with querysets from ListCreateAPIView

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
     
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer(self, *args, **kwargs):
+        return ProductSerializer()
+    
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 class ProductDetail(APIView):
     def get(self, request, id):
