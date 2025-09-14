c2c6607234a3e3b6dfc0ddad29e02fafb20c34c1
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-14 17:09:14 +0200

    [ADD] Finally adding the CollectionList class in urls.py

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:id>/', views.ProductDetail.as_view()),
-    path('collection/', views.collection_list),
+    path('collection/', views.CollectionList.as_view()),
     path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
