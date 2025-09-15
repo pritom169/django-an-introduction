79466caaa772d15a1b6f625ef5e75e9266905669
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-15 12:33:42 +0200

    [ADD] Manual urls replaced by routers

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collection/', views.CollectionList.as_view()),
-    path('collection/<int:pk>/', views.CollectionDetail.as_view())
-]
+urlpatterns = router.urls
