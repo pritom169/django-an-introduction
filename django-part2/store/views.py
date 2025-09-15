87c3cf4fec82b2ab79b78b78373590d5ecbcbbe8
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-15 13:21:38 +0200

    [BUG] Bugs fixed for destroy

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -17,7 +17,7 @@ class ProductViewSet(ModelViewSet):
         return {'request': self.request}
     
     def destroy(self, request, *args, **kwargs):
-        if OrderItem.Objects.filter(product_id=kwargs['pk']).count() > 0:
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error' : 'Product cannot be deleted because it is associated with an order item'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
 
