a5bbd84c0880799be57874d5294db21cb9ccfa62
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-15 15:56:21 +0200

    [ADD] ReviewViewSet added instead of two review function (One for Listing and One for individual reviews)

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -36,3 +32,7 @@ class CollectionViewSet(ModelViewSet):
                 status=status.HTTP_405_METHOD_NOT_ALLOWED
             )
         return super().destroy(request, *args, **kwargs)
+
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
