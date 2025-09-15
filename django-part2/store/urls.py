ec10bc196003109d611399d083f7cc350b809227
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-15 09:46:38 +0200

    [ADD] New Collection view integrated into the urls

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:pk>/', views.ProductDetail.as_view()),
     path('collection/', views.CollectionList.as_view()),
-    path('collection/<int:pk>/', views.collection_detail, name='collection-detail')
+    path('collection/<int:pk>/', views.CollectionDetail.as_view())
 ]
