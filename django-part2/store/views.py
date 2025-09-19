250fc850d7ec513ade527fa6f71aa55aa1c18b71
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-19 20:39:32 +0200

    [ADD] Queryset added for getting reviewlist for a specific product

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -37,5 +37,8 @@ class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
