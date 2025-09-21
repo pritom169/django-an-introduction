e0c29e17585e593c04de01560387029918d0fe0c
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-21 16:43:15 +0200

    [ADD] Adding pagination globally

--- django-part2/storefront/settings.py
+++ django-part2/storefront/settings.py
@@ -142,5 +142,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
