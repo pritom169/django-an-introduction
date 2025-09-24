30a96867e699905f7729e7932181b48e49a35239
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-24 22:25:13 +0200

    [ADD] Adding viewset to the urls

--- django-part2/store/urls.py
+++ django-part2/store/urls.py
@@ -5,6 +5,7 @@ from . import views
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='product')
 router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
