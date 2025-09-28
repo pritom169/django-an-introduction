4ee2e6d87caf932c9afe66316dae4be6952d7994
Pritom Mazumder <pritommazumder@gmail.com>
2025-09-28 19:12:33 +0200

    [ADD] Validating input when product id does not exist

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -69,6 +69,11 @@ class CartSerializer(serializers.ModelSerializer):
 class AddCartItemSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
 
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError('No product with given Id was found.')
+        return value
+
     def save(self, **kwargs):
         cart_id = self.context['cart_id']
         product_id = self.validated_data['product_id']
