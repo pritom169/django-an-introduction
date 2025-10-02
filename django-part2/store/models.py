8c41f446026c7acb2358a94c7a614ed7b0116ac4
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-03 01:21:42 +0200

    [ADD] Connecting user model with customers

--- django-part2/store/models.py
+++ django-part2/store/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -51,6 +52,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
