efe06af28f241a2e885a52d91c535ab298c89f66
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-05 11:39:22 +0200

    [ADD] Installation and related settings for `djoser`

--- django-part2/storefront/settings.py
+++ django-part2/storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "djsoser",
     "playground",
     "debug_toolbar",
     "django_filters",
