34aa7e35ef1f14b4b586ce6e887bc68e34eb428d
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-11 12:38:46 +0200

    [ADD] Customizing JWT authentication token for 1 day validity (access token)

--- django-part2/storefront/settings.py
+++ django-part2/storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,4 +159,8 @@ DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer'
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
