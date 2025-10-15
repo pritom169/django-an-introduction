8153cc8ae851c0e468a9b4c3d081ade34b2fe16f
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-15 22:23:05 +0200

    [ADD] Retrieve the current user

--- django-part2/storefront/settings.py
+++ django-part2/storefront/settings.py
@@ -149,10 +149,6 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
-
 AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
@@ -162,5 +158,6 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
