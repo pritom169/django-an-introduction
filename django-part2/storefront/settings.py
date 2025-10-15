23278e953484d7aae51666845aab1a0dc5343297
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-15 23:14:22 +0200

    [ADD] Adding BaseUserSerializer in setting.py

--- django-part2/storefront/settings.py
+++ django-part2/storefront/settings.py
@@ -153,7 +153,8 @@ AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
     'SERIALIZERS': {
-        'user_create': 'core.serializers.UserCreateSerializer'
+        'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer'
     }
 }
 
