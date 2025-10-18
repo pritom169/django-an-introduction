42454f7e667ace62a7c0b8f0d71032d2dcfd5045
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-18 12:54:41 +0200

    [ADD] Changing permissions to Safe Methods

--- django-part2/store/permissions.py
+++ django-part2/store/permissions.py
@@ -1,8 +1,8 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsAdminOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'GET':
+        if request.method == permissions.SAFE_METHODS:
             return True
         return bool(request.user and request.user.is_staff)
 
