e38271ea9f58bb4775fdcf630c9092c1c603cc55
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-24 12:07:03 +0200

    [ADD] Creation of an order in the order serializer

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -118,11 +118,10 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'placed_at', 'payment_status', 'customer', 'items']
-        
 
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+        Order.objects.create(customer=customer)
