51a4e89fda2488b13ae52720bc74e8d510497007
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-24 09:26:59 +0200

    [ADD] Adding the serializer class for creating card via cart_id

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -118,3 +118,11 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'placed_at', 'payment_status', 'customer', 'items']
+        
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def save(self, **kwargs):
+        print(self.validated_data['cart_id'])
+        print(self.context['user_id'])
