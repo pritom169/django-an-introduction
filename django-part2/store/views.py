b3b83da451572712117bd7d16f4c4a0cef678b6d
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-24 15:31:01 +0200

    [ADD] Create for returning the current  orders

--- django-part2/store/views.py
+++ django-part2/store/views.py
@@ -99,8 +99,12 @@ class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     permission_classes = [IsAuthenticated]
 
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
