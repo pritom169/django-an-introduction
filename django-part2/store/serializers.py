b0fd919a7f243e68b740bf54d5d3ac6e1a1105b9
Pritom Mazumder <pritommazumder@gmail.com>
2025-10-25 12:49:12 +0200

    [ADD] Adding serializer for updating an order

--- django-part2/store/serializers.py
+++ django-part2/store/serializers.py
@@ -153,3 +153,8 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
             return order
+
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Order
+        fields = ['payment_status']
